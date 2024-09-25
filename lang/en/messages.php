d4252a577aaa08acf91b7b3b7c22147e31ca00b1
andreterto-dev <andreterto.dev@gmail.com>
2024-09-24 23:05:27 -0300

    feat: add pt-BR language support

---
+++ lang/en/messages.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'created' => ':model created successfully.',
+    'updated' => ':model updated successfully.',
+    'deleted' => ':model deleted successfully.',
+    'restored' => ':model restored successfully.',
+    'forceDeleted' => ':model successfully permanently deleted.',
+    'not-found' => ':model not found.',
+    'already-exists' => ':model already exists. Please enter another name.',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap our attribute placeholder
+    | with something more reader friendly such as "E-Mail Address" instead
+    | of "email". This simply helps us make our message more expressive.
+    |
+    */
+
+    'attributes' => [
+        'Brand' => 'Brand',
+    ]
+];
---
+++ lang/pt_BR/messages.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'created' => ':model criado(a) com sucesso.',
+    'updated' => ':model atualizado(a) com sucesso.',
+    'deleted' => ':model deletado(a) com sucesso.',
+    'restored' => ':model restaurado(a) com sucesso.',
+    'forceDeleted' => ':model deletado(a) permanentemente com sucesso.',
+    'not-found' => ':model não encontrado(a).',
+    'already-exists' => ':model já existente. Por favor insira outro nome',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap our attribute placeholder
+    | with something more reader friendly such as "E-Mail Address" instead
+    | of "email". This simply helps us make our message more expressive.
+    |
+    */
+
+    'attributes' => [
+        'Brand' => 'Marca',
+    ]
+];
