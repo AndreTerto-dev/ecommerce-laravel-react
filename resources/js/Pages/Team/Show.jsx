39765684545a3ded8eb3b0e74164233478719788
andreterto-dev <andreterto.dev@gmail.com>
2024-10-03 14:09:47 -0300

    feat: add show page for teams

---
+++ resources/js/Pages/Team/Show.jsx
@@ -0,0 +1,69 @@
+import Admin from "@/Layouts/Admin";
+import { Head, Link } from "@inertiajs/react";
+
+export default function Show({ auth, team }) {
+    return (
+        <Admin
+            user={auth.user}
+            header={
+                <div className="flex items-center justify-between">
+                    <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                        {`Team "${team.data.name}"`}
+                    </h2>
+                    <Link
+                        href={route("team.edit", team.data.id)}
+                        className="bg-emerald-500 py-2 px-4 text-white rounded shadow transition-all hover:bg-emerald-600"
+                    >
+                        Edit
+                    </Link>
+                </div>
+            }
+        >
+            <Head title={`Team "${team.data.name}"`} />
+
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg flex flex-row">
+                        <div className="p-6 text-gray-900 dark:text-gray-100">
+                            <div className="grid gap-6">
+                                <div>
+                                    <div>
+                                        <label className="font-bold text-lg">
+                                            Team ID
+                                        </label>
+                                        <p className="mt-1 text-gray-700 dark:text-gray-300">
+                                            {team.data.id}
+                                        </p>
+                                    </div>
+                                    <div className="mt-4">
+                                        <label className="font-bold text-lg">
+                                            Team Name
+                                        </label>
+                                        <p className="mt-1 text-gray-700 dark:text-gray-300">
+                                            {team.data.name}
+                                        </p>
+                                    </div>
+                                    <div className="mt-4">
+                                        <label className="font-bold text-lg">
+                                            Team Country
+                                        </label>
+                                        <p className="mt-1 text-gray-700 dark:text-gray-300">
+                                            {team.data.country}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="ml-auto p-6">
+                            <img
+                                src={team.data.image_path}
+                                alt={`Image of ${team.name}`}
+                                className="w-52 h-52 object-cover"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Admin>
+    );
+}
