e5d1b6a0fbbca54db5b508695a1ac8986980137c
andreterto-dev <andreterto.dev@gmail.com>
2024-10-04 20:41:08 -0300

    feat: create ProductService

---
+++ app/Services/Product/ProductService.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Services\Product;
+
+use App\Exceptions\Product\ProductAlreadyExistsException;
+use App\Exceptions\Product\ProductNotFoundException;
+use App\Models\Product;
+use Illuminate\Http\Response;
+
+class ProductService
+{
+    public function __construct(protected Product $product) {}
+
+    public function index(array $filters = [])
+    {
+        $query = $this->product->query();
+
+        $sortFields = request("sort_field", "created_at");
+        $sortDirection = request("sort_direction", "desc");
+
+        if (isset($filters['name'])) {
+            $query->where('name', 'like', '%' . $filters['name'] . '%');
+        }
+
+        return $products = $query->orderBy($sortFields, $sortDirection)->paginate(5);
+    }
+
+    public function create(array $data)
+    {
+        if ($this->findExistingProductByName($data['name'])) {
+            throw new ProductAlreadyExistsException(__('messages.already-exists', ['model' => modelName('Product')]), Response::HTTP_CONFLICT);
+        }
+
+        return $this->product->create($data);
+    }
+
+    public function getById(int $id)
+    {
+        $product = $this->product->find($id);
+
+        if (!$product) {
+            throw new ProductNotFoundException(__('messages.not-found', ['model' => modelName('Product')]), Response::HTTP_NOT_FOUND);
+        }
+
+        return $product;
+    }
+
+    public function update(int $id, array $data)
+    {
+        $product = $this->getById($id);
+
+        $existingProduct = $this->findExistingProductByName($data['name']);
+
+        if ($existingProduct && $existingProduct->id !== $id) {
+            throw new ProductAlreadyExistsException(
+                __('messages.already-exists', ['model' => modelName('Product')]),
+                Response::HTTP_CONFLICT
+            );
+        }
+
+        $product->update($data);
+
+        return $product;
+    }
+
+    public function destroy(int $id)
+    {
+        $product = $this->getById($id);
+
+        $product->delete();
+
+        return $id;
+    }
+
+    private function findExistingProductByName(string $name)
+    {
+        return $this->product->where('name', $name)->first();
+    }
+}
