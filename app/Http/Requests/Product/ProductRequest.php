17044a2cdb31a78d88981928b19e07ebce885fc8
andreterto-dev <andreterto.dev@gmail.com>
2024-10-04 20:39:16 -0300

    feat: create ProductRequest for validation

---
+++ app/Http/Requests/Product/ProductRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:60',
+            'description' => 'nullable|string|max:200',
+            'price' => 'required|numeric|min:0',
+            'stock_quantity' => 'nullable|integer|min:0',
+            'brand_id' => 'nullable|exists:brands,id',
+            'category_id' => 'required|exists:categories,id',
+            'team_id' => 'required|exists:teams,id',
+        ];
+    }
+}
