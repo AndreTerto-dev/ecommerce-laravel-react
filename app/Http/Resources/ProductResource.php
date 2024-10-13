fd02a30d9079c30aa9b7e55fef7f1712a61c7b0a
andreterto-dev <andreterto.dev@gmail.com>
2024-10-12 23:48:09 -0300

    feat: add images to ProductResource

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class ProductResource extends JsonResource
 {
@@ -23,6 +24,7 @@ class ProductResource extends JsonResource
             'brand' => new BrandResource($this->brand),
             'category' => new CategoryResource($this->category),
             'team' => new TeamResource($this->team),
+            'image_path' => @Storage::url($this->images[0]->image_path) ?? '', 
         ];
     }
 }
