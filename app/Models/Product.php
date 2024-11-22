ca1736b3072a830aecf7fff542d906d0ad736ecf
andreterto-dev <andreterto.dev@gmail.com>
2024-11-22 07:30:39 -0300

    feat: update Product model with new fields and related functions

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Product extends Model
 {
@@ -15,6 +16,9 @@ class Product extends Model
         'name',
         'description',
         'price',
+        'new_price',
+        'installment_count',
+        'installment_value',
         'stock_quantity',
         'launch',
         'discount',
@@ -22,6 +26,7 @@ class Product extends Model
         'brand_id',
         'team_id',
         'category_id',
+        'image_path',
     ];
 
     public function category()
@@ -44,4 +49,28 @@ class Product extends Model
         return $this->hasMany(ProductImage::class, 'product_id');
     }
 
+    // Função para calcular o preço com desconto
+    public function calculateDiscountedPrice()
+    {
+        return $this->discount ? $this->price - ($this->price * ($this->discount / 100)) : $this->price;
+    }
+
+    // Função para calcular o valor das parcelas
+    public function calculateInstallments($newPrice)
+    {
+        $installmentCount = 12; // Define o número de parcelas
+        return $installmentCount > 0 ? $newPrice / $installmentCount : 0;
+    }
+
+    // Evento de criar ou atualizar
+    protected static function booted()
+    {
+        static::saving(function ($product) {
+            // Calcular o preço com desconto
+            $product->new_price = $product->calculateDiscountedPrice();
+
+            // Calcular o valor de cada parcela
+            $product->installment_value = $product->calculateInstallments($product->new_price);
+        });
+    }
 }
