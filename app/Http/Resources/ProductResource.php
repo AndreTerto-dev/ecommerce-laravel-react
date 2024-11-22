2eadb8cc1f8a487c5550a93dde9121bccf58f9e8
andreterto-dev <andreterto.dev@gmail.com>
2024-11-22 07:34:21 -0300

    feat: update ProductResource with new fields

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -5,7 +5,6 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
 
 class ProductResource extends JsonResource
 {
@@ -16,25 +15,22 @@ class ProductResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $newPrice = $this->discount ? $this->price - ($this->price * ($this->discount / 100)) : $this->price;
-
-        $installments = $newPrice / 12;
-
         return [
             'id' => $this->id,
             'name' => $this->name,
             'description' => $this->description,
             'price' => number_format($this->price, 2),
             'discount' => $this->discount,
-            'new_price' => number_format($newPrice, 2),
-            'installments' => number_format($installments, 2),
+            'new_price' => number_format($this->new_price, 2),
+            'installment_count' => $this->installment_count,
+            'installments' => number_format($this->installment_value, 2),
             'slug' => $this->slug,
             'stock_quantity' => $this->stock_quantity,
             'launch' => $this->launch,
             'brand' => new BrandResource($this->brand),
             'category' => new CategoryResource($this->category),
             'team' => new TeamResource($this->team),
-            'image_path' => @Storage::url($this->images[0]->image_path) ?? '',
+            'image_path' => $this->image_path ? Storage::url($this->image_path) : '',
         ];
     }
 }
