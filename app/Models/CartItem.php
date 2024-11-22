1c70b75cda7d49bbe67b014e1512f609bef81aa2
andreterto-dev <andreterto.dev@gmail.com>
2024-11-22 07:43:06 -0300

    feat: create CartItem model

---
+++ app/Models/CartItem.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CartItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['shopping_cart_id', 'product_id', 'quantity'];
+
+    public function cart()
+    {
+        return $this->belongsTo(ShoppingCart::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
