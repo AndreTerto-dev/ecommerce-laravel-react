132444ac4f85aa465423484fa6794b86fd40194d
andreterto-dev <andreterto.dev@gmail.com>
2024-12-04 12:42:55 -0300

    feat: create OrderRequest for validation

---
+++ app/Http/Requests/Order/OrderRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Order;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Symfony\Contracts\Service\Attribute\Required;
+
+class OrderRequest extends FormRequest
+{
+    /**
+     * Obtenha as validações para os dados de entrada.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [ 
+            'total_amount' => 'required|numeric', 
+            'full_name' => 'required|string',
+            'status' => 'nullable|string', 
+            'email' => 'required|email',
+            'cellphone' => 'required|string',  
+            'cpf' => 'required|string',
+
+            'address' => 'required|string',
+            'number' => 'required|string',
+            'neighborhood' => 'required|string',
+            'city' => 'required|string',
+            'state' => 'required|string',
+            'cep' => 'required|string',
+            'country' => 'nullable|string',
+            'shipping_method' => 'nullable|string',
+            'tracking_number' => 'nullable|string',
+            'complement' => 'required|string', 
+        ];
+    }
+}
