e622b2c7e5b852882c1a6bf73f6c0335b75636a9
andreterto-dev <andreterto.dev@gmail.com>
2024-12-04 12:41:18 -0300

    feat: create ShippingDetail model

---
+++ app/Models/ShippingDetail.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ShippingDetail extends Model
+{
+    use HasFactory;
+
+    protected $table = 'shipping_details';
+
+    protected $guarded = [];
+
+    /**
+     * Define o relacionamento com o modelo Order.
+     */
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
