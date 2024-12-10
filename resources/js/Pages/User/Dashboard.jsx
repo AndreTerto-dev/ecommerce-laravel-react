55bf53afec95229019b02519805f06f8867d2d47
andreterto-dev <andreterto.dev@gmail.com>
2024-12-10 10:15:49 -0300

    feat: add cards to dashboard

--- resources/js/Pages/User/Dashboard.jsx
+++ resources/js/Pages/User/Dashboard.jsx
@@ -29,10 +29,69 @@ export default function Dashboard({ products }) {
 
             <Products products={products} />
 
-            {/* Conteúdo do Dashboard */}
-            <div className="dashboard-content mt-8">
-                {/* Adicione seu conteúdo aqui */}
+            <div className="flex gap-6 mx-12 mt-12">
+                <div>
+                    <a href="/brasileiros" className="">
+                        <img
+                            src="/assets/cards/brasileiros.png"
+                            alt="Banner da Loja de Camisas"
+                            className="h-auto rounded-xl"
+                        />
+                    </a>
+                </div>
+                <div>
+                    <a href="/internacionais" className="">
+                        <img
+                            src="/assets/cards/internacionais.png"
+                            alt="Banner da Loja de Camisas"
+                            className="h-auto rounded-xl"
+                        />
+                    </a>
+                </div>
+                <div>
+                    <a href="/selecoes" className="">
+                        <img
+                            src="/assets/cards/selecoes.png"
+                            alt="Banner da Loja de Camisas"
+                            className="h-auto rounded-xl mb-12"
+                        />
+                    </a>
+                </div>
             </div>
+
+            <Products products={products} />
+
+            <div className="flex gap-6 mx-12 mt-12">
+                <div>
+                    <a href="/femininas" className="">
+                        <img
+                            src="/assets/cards/femininas.png"
+                            alt="Banner da Loja de Camisas"
+                            className="h-auto rounded-xl"
+                        />
+                    </a>
+                </div>
+                <div>
+                    <a href="/infantis" className="">
+                        <img
+                            src="/assets/cards/infantis.png"
+                            alt="Banner da Loja de Camisas"
+                            className="h-auto rounded-xl"
+                        />
+                    </a>
+                </div>
+                <div>
+                    <a href="/retros" className="">
+                        <img
+                            src="/assets/cards/retros.png"
+                            alt="Banner da Loja de Camisas"
+                            className="h-auto rounded-xl mb-12"
+                        />
+                    </a>
+                </div>
+            </div>
+
+            <Products products={products} />
         </User>
     );
 }
