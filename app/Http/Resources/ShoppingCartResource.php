e3c70e6fe9a0ebbcc29e0f7a076992cca17a129a
andreterto-dev <andreterto.dev@gmail.com>
2024-12-16 14:43:20 -0300

    feat: update ShoppingCart model to include guest_id

--- app/Http/Resources/ShoppingCartResource.php
+++ app/Http/Resources/ShoppingCartResource.php
@@ -11,6 +11,7 @@ class ShoppingCartResource extends JsonResource
         return [
             'id' => $this->id,
             'user_id' => $this->user_id,
+            'guest_id' => $this->guest_id,
             'total_quantity' => $this->items->sum('quantity'),
             'items' => CartItemResource::collection($this->items),
         ];
