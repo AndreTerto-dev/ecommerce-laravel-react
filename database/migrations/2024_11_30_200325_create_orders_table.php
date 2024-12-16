d1c6a4ec9b1e12c6c4eca55979ab5ca891ae5f9b
andreterto-dev <andreterto.dev@gmail.com>
2024-12-16 14:44:10 -0300

    feat: allow purchases by guests by making user_id nullable in orders migration

--- database/migrations/2024_11_30_200325_create_orders_table.php
+++ database/migrations/2024_11_30_200325_create_orders_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id(); 
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade');
             $table->decimal('total_amount', 10, 2); 
             $table->string('status')->nullable();
             $table->string('email');
