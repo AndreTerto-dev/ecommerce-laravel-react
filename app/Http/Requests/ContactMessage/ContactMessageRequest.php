cb970b18d6cbb4d283ff2e54d666e35379fa285c
andreterto-dev <andreterto.dev@gmail.com>
2024-12-17 11:14:27 -0300

    feat: create ContactMessageRequest for validation

---
+++ app/Http/Requests/ContactMessage/ContactMessageRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests\ContactMessage;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContactMessageRequest extends FormRequest
+{
+    
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'message' => 'required|string|max:2000',
+        ];
+    }
+}
