ae9bff6fdd6b6ec22f97ca4bf6fb19bb78893a3b
andreterto-dev <andreterto.dev@gmail.com>
2025-01-07 14:43:16 -0300

    feat: update FeaturesSection layout for mobile view

--- resources/js/Components/User/FeaturesSection.jsx
+++ resources/js/Components/User/FeaturesSection.jsx
@@ -1,4 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules"; // Módulos necessários
+import "swiper/swiper-bundle.css"; // Estilos do Swiper
+
+const Cards = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 640);
+        };
+
+        handleResize();
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    const cardData = [
+        { href: "/brasileiros", src: "/assets/cards/brasileiros.png" },
+        { href: "/internacionais", src: "/assets/cards/internacionais.png" },
+        { href: "/selecoes", src: "/assets/cards/selecoes.png" },
+    ];
+
+    return isMobile ? (
+        <Swiper
+            spaceBetween={10}
+            slidesPerView={1}
+            grabCursor={true}
+            slidesPerGroup={1} // Número de slides a serem movidos por vez, dependendo da tela
+            freeMode={true}
+        >
+            {cardData.map((card, index) => (
+                <SwiperSlide key={index}>
+                    <div className="">
+                        <a href={card.href}>
+                            <img
+                                src={card.src}
+                                alt={`Banner ${card.href}`}
+                                className="h-auto rounded-xl"
+                            />
+                        </a>
+                    </div>
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    ) : (
+        <div className="flex gap-6 mx-12 mt-12">
+            {cardData.map((card, index) => (
+                <div key={index}>
+                    <a href={card.href}>
+                        <img
+                            src={card.src}
+                            alt={`Banner ${card.href}`}
+                            className="h-auto rounded-xl"
+                        />
+                    </a>
+                </div>
+            ))}
+        </div>
+    );
+};
 
 const FeaturesSection = () => {
     const features = [
@@ -25,25 +89,31 @@ const FeaturesSection = () => {
     ];
 
     return (
-        <div className="flex bg-white py-8 justify-center pb-12 gap-12">
-            {features.map((feature, index) => (
-                <div
-                    key={index}
-                    className="flex items-start space-x-2"
-                >
-                    <img
-                        src={feature.icon}
-                        alt={feature.title}
-                        className="w-14 h-14"
-                    />
-                    <div>
-                        <h3 className="font-semibold text-xl mb-1 text-gray-800">
-                            {feature.title}
-                        </h3>
-                        <p className="text-gray-600 text-sm">{feature.description}</p>
-                    </div>
-                </div>
-            ))}
+        <div className="bg-white py-8">
+            <Swiper
+                modules={[Navigation, Pagination]}
+                spaceBetween={20}
+                slidesPerView={1}
+                pagination={{ clickable: true }}
+            >
+                {features.map((feature, index) => (
+                    <SwiperSlide key={index}>
+                        <div className="flex flex-col items-center text-center space-y-4 mb-14">
+                            <img
+                                src={feature.icon}
+                                alt={feature.title}
+                                className="w-14 h-14"
+                            />
+                            <h3 className="font-semibold text-xl text-gray-800">
+                                {feature.title}
+                            </h3>
+                            <p className="text-gray-600 text-sm">
+                                {feature.description}
+                            </p>
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </div>
     );
 };
