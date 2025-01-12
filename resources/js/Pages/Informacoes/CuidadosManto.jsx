39997df39deb068a7baecbf2b9f5759647c948fc
andreterto-dev <andreterto.dev@gmail.com>
2025-01-12 12:14:11 -0300

    feat: update CuidadosManto for smaller screens

--- resources/js/Pages/Informacoes/CuidadosManto.jsx
+++ resources/js/Pages/Informacoes/CuidadosManto.jsx
@@ -9,13 +9,13 @@ export default function Show() {
             <Header />
 
             <div className="text-center mt-8 mb-6">
-                <h1 className="text-4xl font-bold text-gray-800">
+                <h1 className="sm:text-4xl text-3xl font-bold text-gray-800">
                     Cuidados com o manto
                 </h1>
             </div>
 
             <div className="flex flex-col items-center w-full">
-                <p className="text-2xl font-semibold text-gray-700">
+                <p className="sm:text-2xl text-xl font-semibold text-gray-700">
                     O que fazer para conservar camisas de futebol?
                 </p>
                 <img
@@ -26,10 +26,10 @@ export default function Show() {
             </div>
 
             <div className="max-w-3xl mx-auto px-6 mb-12">
-                <h2 className="text-2xl font-bold text-gray-800 mb-4">
+                <h2 className="sm:text-2xl text-xl font-bold text-gray-800 mb-4">
                     Como lavar sua camisa de futebol
                 </h2>
-                <p className="text-gray-700 text-xl leading-relaxed mb-4">
+                <p className="text-gray-700 sm:text-xl text-lg leading-relaxed mb-4">
                     Para lavar suas camisas de time, você deve optar pelo
                     trabalho manual. Lave as camisas à mão. Além disso, caso
                     seja necessário, você pode deixá-las de molho por poucos
@@ -37,7 +37,7 @@ export default function Show() {
                     tipo de alvejante ou sabão em pó. Para a lavagem, o
                     recomendado é o uso de detergente neutro.
                 </p>
-                <p className="text-gray-700 text-xl leading-relaxed mb-4">
+                <p className="text-gray-700 sm:text-xl text-lg leading-relaxed mb-4">
                     Logo após a lavagem, deixe a camisa secar à sombra, pois o
                     sol pode danificar sua camisa. Por fim, guarde sua camisa em
                     cabides, bem como em um local ventilado, para evitar odores
