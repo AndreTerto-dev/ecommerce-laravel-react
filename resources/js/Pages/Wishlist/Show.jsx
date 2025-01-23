a13de2c036dee235577f25826fb01b0e75a72773
andreterto-dev <andreterto.dev@gmail.com>
2025-01-23 20:02:34 -0300

    feat: update Wishlist for smaller screens

--- resources/js/Pages/Wishlist/Show.jsx
+++ resources/js/Pages/Wishlist/Show.jsx
@@ -7,6 +7,21 @@ import { toast, Toaster } from "sonner";
 export default function Show({ wishlist, success, error, info, warning }) {
     const { delete: deleteItem, post } = useForm();
 
+    const [isMobile, setIsMobile] = useState(false);
+    
+        useEffect(() => {
+            const handleResize = () => {
+                setIsMobile(window.innerWidth < 640);
+            };
+    
+            handleResize();
+            window.addEventListener("resize", handleResize);
+    
+            return () => {
+                window.removeEventListener("resize", handleResize);
+            };
+        }, []);
+
     useEffect(() => {
         if (success) {
             toast.success(success); // Exibe o toast com a mensagem de sucesso
@@ -31,92 +46,224 @@ export default function Show({ wishlist, success, error, info, warning }) {
     };
 
     return (
-        <div className="bg-gray-50 relative">
-            <Header />
+        <>
+            {isMobile ? (
+                <div className="bg-gray-50 relative">
+                    <Header />
 
-            <div className="p-8">
-                <div className="flex justify-center items-start">
-                    {wishlist.items.length === 0 ? (
-                        <p className="text-center text-gray-500">
-                            Sua lista está vazia.
-                        </p>
-                    ) : (
-                        <div className="grid grid-cols-1 md:grid-cols-5 gap-8 w-full">
-                            {wishlist.items.map((item, index) => (
-                                <div
-                                    key={item.product.id}
-                                    className="border rounded-lg p-4 shadow-sm hover:shadow-2xl transition-shadow duration-300 relative"
-                                >
-                                    <span className="text-white bg-[#017bff] py-1 px-2 rounded-lg text-sm font-semibold absolute">
-                                        {item.product.discount}% OFF
-                                    </span>
-                                    <button
-                                        onClick={() =>
-                                            handleRemove(item.product.id)
-                                        }
-                                        className="text-white bg-black hover:bg-red-600 py-1 px-2 rounded-lg text-sm font-semibold absolute ml-52"
-                                    >
-                                        X
-                                    </button>
-
-                                    <a href={`/products/${item.product.slug}`}>
-                                        <img
-                                            src={item.product.image_path}
-                                            alt={item.product.name}
-                                            className="w-full h-auto rounded-lg mb-4 mt-8"
-                                        />
-                                    </a>
-
-                                    <a href={`/products/${item.product.slug}`}>
-                                        <h3 className="text-gray-700 font-bold">
-                                            {item.product.name}
-                                        </h3>
-                                    </a>
-
-                                    <p className="text-sm text-red-500 line-through">
-                                        R${" "}
-                                        {Number(item.product.price)
-                                            .toFixed(2)
-                                            .replace(".", ",")}
-                                    </p>
-                                    <p className="text-green-600 font-bold text-lg">
-                                        R${" "}
-                                        {Number(item.product.new_price)
-                                            .toFixed(2)
-                                            .replace(".", ",")}
-                                    </p>
-                                    <p className="text-sm text-gray-500">
-                                        ou 12x de R${" "}
-                                        {Number(item.product.installment_value)
-                                            .toFixed(2)
-                                            .replace(".", ",")}
-                                    </p>
-                                    <p className="text-xs text-gray-400 mt-2">
-                                        +1453 unidades vendidas
-                                    </p>
-                                    <div className="flex justify-center mt-4">
-                                        <Link
-                                            href={route("cart.add")}
-                                            method="post"
-                                            data={{
-                                                product_id: item.product.id,
-                                                quantity: 1,
-                                            }}
-                                            as="button"
-                                            type="button"
+                    <div className="p-8">
+                        <div className="flex">
+                            {wishlist.items.length === 0 ? (
+                                <p className="text-center text-gray-500">
+                                    Sua lista está vazia.
+                                </p>
+                            ) : (
+                                <div className="grid grid-cols-1 gap-8">
+                                    {wishlist.items.map((item, index) => (
+                                        <div
+                                            key={item.product.id}
+                                            className="border rounded-lg p-4 shadow-md hover:shadow-2xl transition-shadow duration-300"
                                         >
-                                            <button className="px-8 py-3 border rounded-lg bg-[#017bff] hover:bg-[#0164d9] text-white text-lg font-extrabold">
-                                                COMPRAR AGORA
-                                            </button>
-                                        </Link>
-                                    </div>
+                                            <div className="space-x-60 mb-2">
+                                                <span className="text-white bg-[#017bff] py-1 px-2 rounded-lg text-sm font-semibold">
+                                                    {item.product.discount}% OFF
+                                                </span>
+                                                <button
+                                                    onClick={() =>
+                                                        handleRemove(
+                                                            item.product.id
+                                                        )
+                                                    }
+                                                    className="text-white bg-black hover:bg-red-600 py-1 px-2 rounded-lg text-sm font-semibold"
+                                                >
+                                                    X
+                                                </button>
+                                            </div>
+                                            <div className="flex items-center space-x-4">
+                                                <a
+                                                    href={`/products/${item.product.slug}`}
+                                                >
+                                                    <img
+                                                        src={
+                                                            item.product
+                                                                .image_path
+                                                        } // A URL da imagem do produto
+                                                        alt={item.product.name}
+                                                        className="w-32 h-32 object-cover rounded-md"
+                                                    />
+                                                </a>
+                                                <div className="">
+                                                    <a
+                                                        href={`/products/${item.product.slug}`}
+                                                    >
+                                                        <p className="text-base font-semibold text-gray-800">
+                                                            {item.product.name}
+                                                        </p>
+                                                    </a>
+                                                    <div className="flex gap-2">
+                                                        <p className="text-[#017bff] font-bold">
+                                                            R${" "}
+                                                            {item.product
+                                                                .new_price ??
+                                                                item.product
+                                                                    .price}
+                                                        </p>
+                                                        {item.product
+                                                            .new_price && (
+                                                            <p className="text-gray-400 line-through">
+                                                                R${" "}
+                                                                {
+                                                                    item.product
+                                                                        .price
+                                                                }
+                                                            </p>
+                                                        )}
+                                                    </div>
+                                                    <p className="text-sm text-gray-500">
+                                                        ou 12x de R${" "}
+                                                        {Number(
+                                                            item.product
+                                                                .installment_value
+                                                        )
+                                                            .toFixed(2)
+                                                            .replace(".", ",")}
+                                                    </p>
+                                                    <p className="text-xs text-gray-400 mt-2">
+                                                        +1453 unidades vendidas
+                                                    </p>
+                                                    <div className="flex justify-start mt-4">
+                                                        <Link
+                                                            href={route(
+                                                                "cart.add.item"
+                                                            )}
+                                                            method="post"
+                                                            data={{
+                                                                product_id:
+                                                                    item.product
+                                                                        .id,
+                                                                quantity: 1,
+                                                            }}
+                                                            as="button"
+                                                            type="button"
+                                                        >
+                                                            <button className="px-6 py-3 border rounded-lg bg-[#017bff] hover:bg-[#0164d9] text-white text-base font-extrabold">
+                                                                COMPRAR AGORA
+                                                            </button>
+                                                        </Link>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))}
                                 </div>
-                            ))}
+                            )}
                         </div>
-                    )}
+                    </div>
+                    <Footer />
                 </div>
-            </div>
-            <Footer />
-        </div>
+            ) : (
+                <div className="bg-gray-50 relative">
+                    <Header />
+
+                    <div className="p-8">
+                        <div className="flex justify-center items-start">
+                            {wishlist.items.length === 0 ? (
+                                <p className="text-center text-gray-500">
+                                    Sua lista está vazia.
+                                </p>
+                            ) : (
+                                <div className="grid grid-cols-1 md:grid-cols-5 gap-8 w-full">
+                                    {wishlist.items.map((item, index) => (
+                                        <div
+                                            key={item.product.id}
+                                            className="border rounded-lg p-4 shadow-sm hover:shadow-2xl transition-shadow duration-300 relative"
+                                        >
+                                            <span className="text-white bg-[#017bff] py-1 px-2 rounded-lg text-sm font-semibold absolute">
+                                                {item.product.discount}% OFF
+                                            </span>
+                                            <button
+                                                onClick={() =>
+                                                    handleRemove(
+                                                        item.product.id
+                                                    )
+                                                }
+                                                className="text-white bg-black hover:bg-red-600 py-1 px-2 rounded-lg text-sm font-semibold absolute ml-52"
+                                            >
+                                                X
+                                            </button>
+
+                                            <a
+                                                href={`/products/${item.product.slug}`}
+                                            >
+                                                <img
+                                                    src={
+                                                        item.product.image_path
+                                                    }
+                                                    alt={item.product.name}
+                                                    className="w-full h-auto rounded-lg mb-4 mt-8"
+                                                />
+                                            </a>
+
+                                            <a
+                                                href={`/products/${item.product.slug}`}
+                                            >
+                                                <h3 className="text-gray-700 font-bold">
+                                                    {item.product.name}
+                                                </h3>
+                                            </a>
+
+                                            <p className="text-sm text-red-500 line-through">
+                                                R${" "}
+                                                {Number(item.product.price)
+                                                    .toFixed(2)
+                                                    .replace(".", ",")}
+                                            </p>
+                                            <p className="text-green-600 font-bold text-lg">
+                                                R${" "}
+                                                {Number(item.product.new_price)
+                                                    .toFixed(2)
+                                                    .replace(".", ",")}
+                                            </p>
+                                            <p className="text-sm text-gray-500">
+                                                ou 12x de R${" "}
+                                                {Number(
+                                                    item.product
+                                                        .installment_value
+                                                )
+                                                    .toFixed(2)
+                                                    .replace(".", ",")}
+                                            </p>
+                                            <p className="text-xs text-gray-400 mt-2">
+                                                +1453 unidades vendidas
+                                            </p>
+                                            <div className="flex justify-center mt-4">
+                                                <Link
+                                                    href={route(
+                                                        "cart.add.item"
+                                                    )}
+                                                    method="post"
+                                                    data={{
+                                                        product_id:
+                                                            item.product.id,
+                                                        quantity: 1,
+                                                    }}
+                                                    as="button"
+                                                    type="button"
+                                                >
+                                                    <button className="px-8 py-3 border rounded-lg bg-[#017bff] hover:bg-[#0164d9] text-white text-lg font-extrabold">
+                                                        COMPRAR AGORA
+                                                    </button>
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                    <Footer />
+                </div>
+            )}
+        </>
     );
 }
