721d8fcba06a42b7ae09ffe184576a4c55f346a4
andreterto-dev <andreterto.dev@gmail.com>
2025-02-26 07:54:43 -0300

    feat: add additional fields to orders migration

--- database/migrations/2024_11_30_200325_create_orders_table.php
+++ database/migrations/2024_11_30_200325_create_orders_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id(); 
             $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade');
-            $table->decimal('total_amount', 10, 2); 
+            $table->decimal('total_amount', 10, 2);
+            $table->string('size');
+            $table->string('personalization');
             $table->string('status')->nullable();
             $table->string('email');
             $table->string('full_name');
