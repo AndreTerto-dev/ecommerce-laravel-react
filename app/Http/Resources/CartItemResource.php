969a2603e41185c210c8c56225fab5eaa532fd1c
andreterto-dev <andreterto.dev@gmail.com>
2025-02-26 07:51:55 -0300

    feat: update CartItemResource with new fields

--- app/Http/Resources/CartItemResource.php
+++ app/Http/Resources/CartItemResource.php
@@ -11,8 +11,16 @@ class CartItemResource extends JsonResource
         return [
             'id' => $this->id,
             'product_id' => $this->product_id,
+            'name' => $this->name,
             'quantity' => $this->quantity,
             'product' => new ProductResource($this->product),
+            'price' => number_format($this->product->price, 2),
+            'new_price' => number_format($this->product->new_price, 2),
+            'installment_count' => $this->product->installment_count,
+            'installments' => number_format($this->product->installment_value, 2),
+            'size' => $this->size,
+            'personalization' => $this->personalization,
+            'image_path' => $this->image_path,
         ];
     }
 }
